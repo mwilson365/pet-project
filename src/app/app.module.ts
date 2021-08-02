7570bcefad494ffdd7ea79d5b363befee92ca4e4
Emily Mikl <emikl@glhec.org>
2021-08-02 16:34:34 -0500

    Break out navigation into own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { UAppBarModule, UButtonModule, UDrawerModule } from '@nelnet/unifi-compo
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
